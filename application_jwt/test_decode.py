da89781a775f03c8400b7b019800c689a5dcae22
alice.nascimento@bsatech.io <alice.nascimento@bsatech.io>
2026-02-02 16:54:27 -0300

    feat: unit test for validating the JWT token

--- application_jwt/test_decode.py
+++ application_jwt/test_decode.py
@@ -5,6 +5,7 @@ import base64
 from main import user_register, users
 from main import CHAVE, ALGORITMO
 
+
 user_register("Alice", "12345")
 
 @pytest.mark.unit
@@ -15,41 +16,37 @@ def test_validation_users():
     assert len(users) > 0
 
 @pytest.mark.unit
-def test_user_decode_jwt():
+def test_token_validation():
+    token = user_register("artur", "23456")
 
-    token = user_register("ALICE", "EMILY")
+    decoded = jwt.decode(
+        token,
+        CHAVE,
+        algorithms=[ALGORITMO]
+    )
 
-    headers_b64, payload_b64, signature = token.split(".")
-    payload_b = base64.urlsafe_b64decode(payload_b64 + '==')
-    payload = json.loads(payload_b)
+    assert decoded["user"] == "artur"
+    assert decoded["password"] == "23456"
 
-    payload["user"] = "EU"
-    payload["password"] = "AQUI"
-
-    new_payload_b64 = base64.urlsafe_b64encode(json.dumps(payload).encode()).decode().rstrip('=')
-    print(new_payload_b64)
-    print("_______")
-    print(token)
-
-    assert new_payload_b64 != token
 
 @pytest.mark.unit
 def test_user_register_jwt():
     '''
     testa se houve modificação no payload ou chave do token
     '''
-    token = user_register("Alice", "123")
+    token = user_register("Jose", "123")
 
-    headers_b64, payload_b64, signature = token.split(".")
+    headers_b64, payload_b64, signature = token.split(".") #diivde o token
  
-    payload_b = base64.urlsafe_b64decode(payload_b64 + '==')
+    payload_b = base64.urlsafe_b64decode(payload_b64 + '==') #decodificando o payload
     payload = json.loads(payload_b)
  
-    payload['user']= "AAAA"
+    payload['user']= "Lucas" #modifica o payload
  
-    new_payload_b64 = base64.urlsafe_b64encode(
+    new_payload_b64 = base64.urlsafe_b64encode( #remonta o token
         json.dumps(payload).encode()
     ).decode().rstrip("=")
+
 
     with pytest.raises(jwt.InvalidSignatureError):
         '''
